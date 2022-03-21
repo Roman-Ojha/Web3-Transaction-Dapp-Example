3407efb4bf3a5b5eed2e7026edff6e63f1076437
Roman Ojha <razzroman98@gmail.com>
2022-03-21 11:16:11 +0545

    getting receiver address input field data

--- web/src/App.js
+++ web/src/App.js
@@ -4,7 +4,11 @@ import Web3 from "web3";
 import Transaction from "./contracts/Transaction.json";
 
 const App = () => {
-  const [pageData, setPageData] = useState({});
+  const [pageData, setPageData] = useState({
+    senderBalance: 0,
+    receiverBalance: 0,
+    receiverAddress: "",
+  });
   useEffect(async () => {
     try {
       const web3 = new Web3("http://127.0.0.1:7545");
@@ -14,20 +18,42 @@ const App = () => {
         Transaction.abi,
         deployedNetwork.address
       );
-      const address = (await web3.eth.getAccounts())[0];
+      const address = (await web3.eth.getAccounts())[1];
       const senderBalance = await contract.methods.getBalance(address).call();
-      console.log(senderBalance);
-    } catch (err) {}
+      setPageData({
+        ...pageData,
+        senderBalance: (senderBalance * 10 ** -18).toFixed(3),
+      });
+    } catch (err) {
+      console.log(err);
+    }
   }, []);
+  const sendTransaction = () => {
+    console.log(pageData.receiverAddress);
+  };
   return (
     <>
       <div className="Page_Container">
         <div className="Sender_Container">
           <h1 className="Sender_Info_Text">Sender Info</h1>
-          <h2 className="Sender_Balance">0Eth</h2>
+          <h2 className="Sender_Balance">{pageData.senderBalance}Eth</h2>
           <h2 className="Send_Ether_Text">Send Ether</h2>
-          <input type="text" placeholder="Receiver Address" />
-          <button className="Sender_Send_Button" type="submit">
+          <input
+            type="text"
+            placeholder="Receiver Address"
+            onChange={(e) => {
+              setPageData({
+                ...pageData,
+                receiverAddress: e.target.value,
+              });
+            }}
+            value={pageData.receiverAddress}
+          />
+          <button
+            className="Sender_Send_Button"
+            type="submit"
+            onClick={sendTransaction}
+          >
             Send
           </button>
           <button className="Sender_Refresh_Button" type="submit">
