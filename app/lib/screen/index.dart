231b1d80a42ab1a5eca498d16392d44a30c394eb
Roman Ojha <razzroman98@gmail.com>
2022-03-22 14:44:11 +0545

    completed design of flutter app

--- app/lib/screen/index.dart
+++ app/lib/screen/index.dart
@@ -16,110 +16,121 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       body: Container(
         width: double.infinity,
-        height: double.infinity,
-        decoration: const BoxDecoration(
-          color: Color.fromRGBO(3, 32, 60, 1),
-        ),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Container(
-                width: 300,
-                height: 350,
-                decoration: const BoxDecoration(
-                  color: Color.fromRGBO(99, 97, 102, 1),
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(10),
-                  ),
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    const Text(
-                      "Sender Info",
-                      style: TextStyle(fontSize: 20.0, color: Colors.white),
-                    ),
-                    const Text(
-                      "0 Eth",
-                      style: TextStyle(fontSize: 15.0, color: Colors.white),
-                    ),
-                    const Text(
-                      "Send Ether",
-                      style: TextStyle(fontSize: 15.0, color: Colors.white),
-                    ),
-                    SizedBox(
-                      width: 250.0,
-                      child: TextFormField(
-                        decoration: const InputDecoration(
-                          labelText: "Receiver Address",
-                          contentPadding: EdgeInsets.only(left: 10.0),
-                        ),
-                      ),
-                    ),
-                    Row(
-                      children: [
-                        SizedBox(
-                          width: 100.0,
-                          child: TextFormField(
-                            decoration: const InputDecoration(
-                              labelText: "Amount in Wei",
-                              contentPadding: EdgeInsets.only(left: 10.0),
-                            ),
-                          ),
-                        ),
-                        const Text(
-                          "< = 1 Eth",
-                          style: TextStyle(
-                            fontSize: 15.0,
-                            color: Colors.white,
-                          ),
-                        )
-                      ],
-                    ),
-                    TextButton(
-                      onPressed: () {},
-                      child: const Text(
-                        "Send",
-                        style: TextStyle(
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                  ],
+        decoration: const BoxDecoration(color: Color.fromRGBO(3, 32, 60, 1)),
+        child: ListView(
+          children: [
+            const SizedBox(
+              height: 30.0,
+            ),
+            Container(
+              margin: const EdgeInsets.only(
+                left: 30.0,
+                right: 30.0,
+              ),
+              height: 350,
+              decoration: const BoxDecoration(
+                color: Color.fromRGBO(99, 97, 102, 1),
+                borderRadius: BorderRadius.all(
+                  Radius.circular(10),
                 ),
               ),
-              Container(
-                width: 300,
-                height: 150,
-                decoration: const BoxDecoration(
-                  color: Color.fromRGBO(99, 97, 102, 1),
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(10),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  const Text(
+                    "Sender Info",
+                    style: TextStyle(fontSize: 20.0, color: Colors.white),
                   ),
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: const [
-                    Text(
-                      "Receiver Info",
+                  const Text(
+                    "0 Eth",
+                    style: TextStyle(fontSize: 15.0, color: Colors.white),
+                  ),
+                  const Text(
+                    "Send Ether",
+                    style: TextStyle(fontSize: 15.0, color: Colors.white),
+                  ),
+                  SizedBox(
+                    width: 250.0,
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        labelText: "Receiver Address",
+                        contentPadding: EdgeInsets.only(left: 10.0),
+                        fillColor: Colors.white,
+                        filled: true,
+                      ),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      SizedBox(
+                        width: 150.0,
+                        child: TextFormField(
+                          decoration: const InputDecoration(
+                            labelText: "Amount in Wei",
+                            contentPadding: EdgeInsets.only(left: 10.0),
+                            fillColor: Colors.white,
+                            filled: true,
+                          ),
+                        ),
+                      ),
+                      const Text(
+                        "< = 1 Eth",
+                        style: TextStyle(
+                          fontSize: 15.0,
+                          color: Colors.white,
+                        ),
+                      )
+                    ],
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      "Send",
                       style: TextStyle(
-                        fontSize: 20.0,
                         color: Colors.white,
                       ),
                     ),
-                    Text(
-                      "0 Eth",
-                      style: TextStyle(
-                        fontSize: 15.0,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ],
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 30.0,
+            ),
+            Container(
+              margin: const EdgeInsets.only(
+                left: 30.0,
+                right: 30.0,
+              ),
+              height: 150,
+              decoration: const BoxDecoration(
+                color: Color.fromRGBO(99, 97, 102, 1),
+                borderRadius: BorderRadius.all(
+                  Radius.circular(10),
                 ),
-              )
-            ],
-          ),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: const [
+                  Text(
+                    "Receiver Info",
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Text(
+                    "0 Eth",
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
         ),
       ),
     );
